27f7776fd5b90135e77c6bbb9b258bd2d759177d
Артур <Artur.Bakirov@moex.com>
2019-05-22 10:27:36 +0300

    init dump.py

---
+++ dump.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#BakirovAR turkin86@mail.ru
+import tarfile
+import os
+import sys
+import shutil
+import logging
+from datetime import datetime
+
+def get_jobs():
+    print 'jobs'
+def main():
+    print 'hello'
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
