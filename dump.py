f13a1140a0131ebd7f937e8491c2c3789fcb02ef
Бакиров Артур Рифович <artur.bakirov@moex.com>
2019-05-23 13:24:11 +0300

    get all list return files

--- dump.py
+++ dump.py
@@ -33,7 +33,8 @@ def get_files(root_path, files):
         if is_dir(path_file):
             logging.info("dir: %s" % (path_file))
             chld_files = ls_dir(path_file)            
-            get_files(path_file, chld_files)
+            dir_files = get_files(path_file, chld_files)
+            file_list.append(dir_files)
         else:
             logging.info("file: %s" % (path_file))
             file_list.append(path_file)
