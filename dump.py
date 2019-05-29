12552e083c26aced0d0dd2933946048fa395187f
Бакиров Артур Рифович <artur.bakirov@moex.com>
2019-05-29 10:32:30 +0300

    fix filter depth

--- dump.py
+++ dump.py
@@ -91,7 +91,7 @@ def get_files(path, list_files=None, db_files=[], recursive=False, depth=0):
             #recursive block
             if recursive and is_dir(path_file):
                 chld_files = ls_dir(path_file)
-                get_files(path_file, list_files=chld_files, db_files=db_files, recursive=True)
+                get_files(path_file, list_files=chld_files, db_files=db_files, recursive=True, depth=depth)
                 #logging.info("dir: %s" % (path_file))
             #else:
                 #logging.info("file: %s" % (path_file))
