79f67b132aaa814e1ccd4430469344dac523d49d
Бакиров Артур Рифович <artur.bakirov@moex.com>
2019-05-30 16:18:46 +0300

    load testing

--- dump.py
+++ dump.py
@@ -146,7 +146,9 @@ def get_files(path, list_files=None, db_files=[], recursive=False, depth=0, patt
             #     #logging.info("dir: %s" % (path_file))
             # else:
             #     logging.info("file: %s" % (path_file))
-            db_files = filter_files([path_file], patterns)
+                if is_patterns(path_file, patterns) and is_duplicate(patterns, db_files):
+                    db_files.append(path_file)
+                    logging.info("add file: %s" % (path_file))
 
 def ls_dir(path):
     return os.listdir(path)
