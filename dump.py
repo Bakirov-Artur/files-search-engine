c59d56a02440cd8226a5bce4be3d83048d831c63
Бакиров Артур Рифович <artur.bakirov@moex.com>
2019-05-31 16:26:42 +0300

    fixd get files

--- dump.py
+++ dump.py
@@ -136,16 +136,16 @@ def get_dir_files(path, files=None):
     elif is_dir(path):    
         ls = ls_dir(path)
         if not ls:
-            fls = path
+            return path
         fls = ls
     return fls
 
 
 def get_files(path, list_files=None, db_files=[], recursive=False, depth=0, patterns=None):
     files = get_dir_files(path, files=list_files);
-    if isinstance(files, basestring) and is_it_possible_add(path, patterns, files=db_files):
-        print path
-        db_files.append(path)
+    if isinstance(files, basestring) and is_it_possible_add(files, patterns, files=db_files):
+        print files
+        db_files.append(files)
     else:
         for file in files:
             path_file = None
