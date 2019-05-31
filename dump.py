552b2781ec005de20a4480259f351c1d8e1c65a9
Бакиров Артур Рифович <artur.bakirov@moex.com>
2019-05-31 10:53:04 +0300

    load json data

--- dump.py
+++ dump.py
@@ -61,7 +61,7 @@ def main(data):
     logging.basicConfig(level=logging.INFO)
     dp = data.get("dump")
     print dp
-    archive_file = ''.join(dp.get('path'), dp.get('type'))
+    archive_file = ''.join([dp.get('path'), dp.get('type')])
     items = dp.get('items')
     archive_flist = []
     for item in items:
